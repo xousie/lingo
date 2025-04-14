344091c72c9e4be5e998bf4e0bc173fa9f584b81
xousie <125357803+xousie@users.noreply.github.com>
2025-04-14 20:28:58 +0700

    marketing footer, /learn page init (web/mobile)

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,9 @@
+const LearnPage = () => {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <p>Welcome to the Learn page!</p>
+    </div>
+  );
+};
+export default LearnPage;
--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="">
+          <Image
+            src="/CN.svg"
+            alt="Mandarin"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md  drop-shadow-lg"
+          />
+          Mandarin
+        </Button>
+        <Button size="lg" variant="ghost" className="">
+          <Image
+            src="/JP.svg"
+            alt="Japanese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md  drop-shadow-lg"
+          />
+          Japanese
+        </Button>
+        <Button size="lg" variant="ghost" className="">
+          <Image
+            src="/US.svg"
+            alt="English"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md  drop-shadow-lg"
+          />
+          English
+        </Button>
+        <Button size="lg" variant="ghost" className="">
+          <Image
+            src="/ES.svg"
+            alt="Spanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md  drop-shadow-lg"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="">
+          <Image
+            src="/ID.svg"
+            alt="Bahasa"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md  drop-shadow-lg"
+          />
+          Bahasa
+        </Button>
+      </div>
     </footer>
   );
 };
