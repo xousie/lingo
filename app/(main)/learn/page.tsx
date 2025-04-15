be191e26e2c6859e7383f059ebb28eb675fd81f6
xousie <125357803+xousie@users.noreply.github.com>
2025-04-15 22:54:50 +0700

    learn page skeleton, logout(userButton bottom of sidebar) then start of database

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,22 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { Header } from "./header";
+
 const LearnPage = () => {
   return (
-    <div>
-      <h1>Learn</h1>
-      <p>Welcome to the Learn page!</p>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/ES.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
